c5d3cdb66123a11d508ec914ca2f2c4e734b00a1
Divyatech-hub <divyalakshmi5804@gmail.com>
2023-10-21 03:08:31 -0400

    added the splash screen logo and button

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class SplashScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      splashVisible: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ splashVisible: false });
+    }, 3000); // Change this value to control how long the splash screen is displayed (in milliseconds)
+  }
+
+  render() {
+    return this.state.splashVisible ? (
+      <View style={styles.container}>
+        <Image
+          source={require("../stepup/assets/stepergize.png")} // Replace with your logo image path
+          style={styles.logo}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.setState({ splashVisible: false })}
+        >
+          <Text style={styles.buttonText}>Start</Text>
+        </TouchableOpacity>
+      </View>
+    ) : (
+      // Your main app content goes here
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Main App Content</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#007bff',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
   },
 });
+
+export default SplashScreen;
