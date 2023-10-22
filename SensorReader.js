d0f7ede0f89c971f968c78444190ef3604fbb6a4
nguy1056 <nguy1056@msu.edu>
2023-10-22 11:53:58 +0700

    dummy data instead of sensor

--- SensorReader.js
+++ SensorReader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/* import React, { Component } from 'react';
 import SerialPort from 'react-native-serialport';
 
 class SensorReader extends Component {
@@ -28,6 +28,43 @@ class SensorReader extends Component {
 
   render() {
     null;
+  }
+}
+
+export default SensorReader;
+*/
+import React, { Component } from 'react';
+
+class SensorReader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sensorValue: this.getRandomFloat(20, 100), // Initialize with a random value
+    };
+  }
+
+  componentDidMount() {
+    // Simulate data updates at regular intervals (e.g., every 3 seconds)
+    this.dataUpdateInterval = setInterval(() => {
+      this.setState({ sensorValue: this.getRandomFloat(20, 100) });
+    }, 3000); // Change the interval as needed
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.dataUpdateInterval); // Clear the interval when unmounting the component
+  }
+
+  // Generate a random float value between min and max (inclusive)
+  getRandomFloat(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(2);
+  }
+
+  getDistance() {
+    return this.state.sensorValue;
+  }
+
+  render() {
+    return null; // This component doesn't render anything
   }
 }
 
